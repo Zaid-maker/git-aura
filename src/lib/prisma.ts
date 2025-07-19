6ef2ba177636075a3762466d9daefcd95352ab74
Anshkaran7 <karanchandravanshi217@gmail.com>
2025-07-19 17:01:41 +0530

    Update package dependencies and remove unused SQL migration files
    - Added dotenv package for environment variable management in package.json and package-lock.json.
    - Updated build and postinstall scripts in package.json to use npx for Prisma generation.
    - Removed obsolete SQL migration and setup files to streamline the project structure.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,33 +1,13 @@
 import { PrismaClient } from "@prisma/client";
-import { dbConfig } from "./db.config";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const prismaClient = new PrismaClient({
-  datasources: {
-    db: {
-      url: dbConfig.url,
-    },
-  },
-  log:
-    process.env.NODE_ENV === "development"
-      ? ["query", "error", "warn"]
-      : ["error"],
-});
-
-async function testConnection() {
-  try {
-    await prismaClient.$connect();
-    console.log("✅ Prisma connected to the database.");
-  } catch (error) {
-    console.error("❌ Failed to connect to the database with Prisma:", error);
-  }
-}
-
-testConnection();
-
-export const prisma = globalForPrisma.prisma ?? prismaClient;
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query"], // optional
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
